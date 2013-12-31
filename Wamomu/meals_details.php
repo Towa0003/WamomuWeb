1098235b93697874185a68c772f779a48e205e65
Thundernator <paulus131313@yahoo.de>
2013-12-31 16:09:11 +0100

    kp

---
+++ Wamomu/meals_details.php
@@ -0,0 +1,20 @@
+<?php
+$host="localhost"; //replace with database hostname 
+$username="wamomu"; //replace with database username 
+$password="wamomu"; //replace with database password 
+$db_name="wamomu"; //replace with database name
+ 
+$con=mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+$sql = "select * from meals"; 
+$result = mysql_query($sql);
+$json = array();
+ 
+if(mysql_num_rows($result)){
+while($row=mysql_fetch_assoc($result)){
+$json['meals'][]=$row;
+}
+}
+mysql_close($con);
+echo json_encode($json); 
+?> 
