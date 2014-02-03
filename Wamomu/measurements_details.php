108dec88aa176d980568f6315c865e65dcc60e43
Thundernator <paulus131313@yahoo.de>
2014-02-03 22:35:52 +0100

    Canvas

---
+++ Wamomu/measurements_details.php
@@ -0,0 +1,21 @@
+<?php
+$host="localhost"; //replace with database hostname 
+$username="wamomu"; //replace with database username 
+$password="wamomu"; //replace with database password 
+$db_name="wamomu"; //replace with database name
+ 
+$con=mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysql_select_db("$db_name")or die("cannot select DB");
+$sql = "select * from measurements"; 
+$result = mysql_query($sql);
+$json = array();
+ 
+if(mysql_num_rows($result)){
+while($row=mysql_fetch_assoc($result)){
+//$json['measurements'][]=$row;
+    $json[]=$row;
+}
+}
+mysql_close($con);
+echo json_encode($json); 
+?> 
