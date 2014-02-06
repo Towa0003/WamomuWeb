a410acafa98bd627a39ddf929c2b4f0c7bf2ea2b
Thundernator <paulus131313@yahoo.de>
2014-02-06 15:18:34 +0100

    Richtige ausgabe in Mahlzeit,Messungen,Übersicht

--- Wamomu/js/content.js
+++ Wamomu/js/content.js
@@ -67,8 +67,11 @@
                 $('ul').empty();
                 $.each(data, function (key, val) {
                     if (val.date == i) {
-                        $('ul').append('<li id="' + key + '">' + val.mealkind + ' ' + val.meal + ' </li>');
+                        $('#contentMealul').append('<li id="' + key + '">' + val.mealkind + ' ' + val.meal + ' </li>');
+                        $('#contentMeasurementsul').append('<li id="' + key + '">' + val.time + ' ' + val.mvalue + ' </li>');
                         // $('div[id=Januar]').append('<button onclick="buttonClick(' + "'" + val.date + "'" + ');">1.</button>');
+                        $('#contentOverviewUl').append('<li id="' + key + '">' + val.mealkind + ' ' + val.meal + ' </li>');
+                        $('#contentOverviewUl').append('<li id="' + key + '">' + val.time + ' ' + val.mvalue + ' </li>');
                     }
                 });
             });
