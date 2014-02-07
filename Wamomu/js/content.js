314bbca61a7a1150182668d6ec9d8e7373db2a17
Christian <eaglechris@hotmail.de>
2014-02-07 02:00:00 +0100

    Changes on Canvas and Layout
    Only the current or clicked Month will be drawn; The current or clicked
    day will be highlited; Some Layout changes

--- Wamomu/js/content.js
+++ Wamomu/js/content.js
@@ -53,13 +53,11 @@
                     oldJahr = jahr;
                 });
             });
-
-            measurements();
-
-            var days = new Date().getFullYear() % 4 == 0 ? 366 : 365;
-
-            //            alert(days);
-
+            //            Date-Objekt um die Messwerte des aktuellen Monats anzuzeigen
+            var today = new Date();
+            var d = today.getDate();
+            var m = today.getMonth() + 1;
+            measurements(d, m);
         }
 
         function buttonClick(i) {
@@ -82,8 +80,10 @@
                         $('#contentOverviewUl').append('<li id="' + key + '">' + (val.time).substring(0, 5) + ' Uhr    -    Messwert: ' + val.mvalue + ' </li>');
                     }
                 });
+                measurements(i.substring(8, 10), i.substring(5, 7));
 
             });
+
         }
 
         function json() {
@@ -100,9 +100,9 @@
             var c = document.getElementById("myCanvas");
             var ctx = c.getContext("2d");
             ctx.beginPath();
-            for (var i = 0; i < 7; i++) {
+            for (var i = 0; i < 9; i++) {
                 ctx.moveTo(0, i * 30);
-                ctx.lineTo(2000, i * 30);
+                ctx.lineTo(1300, i * 30);
             }
 
             ctx.strokeStyle = '#919191'
@@ -113,52 +113,76 @@
 
         }
 
-        function measurements() {
-            grid();
+        function measurements(day, month) {
+            console.log("ONCLICK: " + day + ". " + month);
             var start = true;
             var array = [];
             var c = document.getElementById("myCanvas");
             var ctx = c.getContext("2d");
+            ctx.clearRect(0, 0, c.width, c.height);
+            grid();
             ctx.font = "10px sans-serif";
-            var i = 0;
+            var i = 10;
 
             $.getJSON('/Wamomuweb/wamomu/php/measurements_details.php', function (data) {
                 /* data will hold the php array as a javascript object */
-
                 ctx.strokeStyle = '#65A6D1'
                 ctx.beginPath();
                 $.each(data, function (key, val) {
-                    var tag = (val.date).substring(8, 10);
-                    array.push(val.mvalue);
-                    var temp = Math.round(val.mvalue);
-                    console.log(temp + "   " + i);
+                    // Wenn der übergebene Monat dem der Werte aus der Datenbank entspricht -> Linien zeichnen
+                    if (month == (val.date).substring(5, 7)) {
+                        var tag = (val.date).substring(8, 10);
+                        array.push(val.mvalue);
+                        var temp = Math.round(val.mvalue);
+                        console.log(temp + "   " + i);
 
-                    ctx.lineTo(i, 250 - (temp * 2));
-                    ctx.lineWidth = 2.5;
+                        // Linien zu den Messwerten zeichnen
+                        ctx.lineTo(i, 250 - (temp * 2));
+                        ctx.lineWidth = 2.5;
 
-                    ctx.fillText(tag, i, 190);
-                    ctx.fillText((val.time).substring(0, 5), i - 8, 200);
-                    i += 40;
-                    console.log(val.mvalue);
+                        // Tag und Uhrzeit an Graphen schreiben
+                        ctx.fillStyle = "#000";
+                        ctx.fillText(tag, i, 190);
+                        ctx.fillText((val.time).substring(0, 5), i - 8, 200);
+                        i += 40;
+                    } else {
+                        // Ansonsten nichts zeichnen
+                    }
                 });
                 ctx.stroke();
                 ctx.closePath();
 
-                i = 0;
-                ctx.fillStyle = "#65A6D1"; // Füllung Kreis
-                ctx.strokeStyle = '#108CDE'; // Kontur Kreis
+                // Punkte an den Messwerten zeichnen
+                i = 10;
                 $.each(data, function (key, val) {
-                    array.push(val.mvalue);
-                    var temp = Math.round(val.mvalue);
+                    // Wenn der übergebene Monat dem der Werte aus der Datenbank entspricht -> Punkte an Messwert zeichnen
+                    if (month == (val.date).substring(5, 7)) {
+                        array.push(val.mvalue);
+                        var temp = Math.round(val.mvalue);
+                        // Angeklickter Tag wird hervorgehoben
+                        if (day == (val.date).substring(8, 10)) {
+                            ctx.fillStyle = "#00FF00"; // Füllung Kreis
+                            ctx.strokeStyle = '#006300'; // Kontur Kreis
+                            ctx.beginPath();
+                            ctx.arc(i, 250 - (temp * 2), 6, 2 * Math.PI, false);
+                            ctx.closePath();
+                        }
+                        // Ansonsten andere Tage werden normal dargestellt
+                        else {
+                            ctx.fillStyle = "#65A6D1"; // Füllung Kreis
+                            ctx.strokeStyle = '#2B7CB3'; // Kontur Kreis
+                            ctx.beginPath();
+                            ctx.arc(i, 250 - (temp * 2), 6, 2 * Math.PI, false);
+                            ctx.closePath();
+                        }
+                        ctx.fill();
+                        ctx.lineWidth = 1;
+                        ctx.stroke();
+                        i += 40;
 
-                    ctx.beginPath();
-                    ctx.arc(i, 250 - (temp * 2), 6, 2 * Math.PI, false);
-                    ctx.closePath();
-
-                    ctx.fill();
-                    ctx.lineWidth = 1;
-                    ctx.stroke();
-                    i += 40;
+                    } else {
+                        // Ansonsten nichts zeichnen
+                    }
                 });
             });
 
